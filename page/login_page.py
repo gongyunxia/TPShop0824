ba139165526b518a7d1c2a1fa0991f0cb6902998
gongyunxia <18952555601@163.com>
2018-08-24 21:15:45 +0800

    添加了测试步骤

--- page/login_page.py
+++ page/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from base.base_action import BaseAction
@@ -8,11 +9,14 @@ class LoginPage(BaseAction):
     password_edit_text = By.ID, "com.tpshop.malls:id/edit_password"
     login_button = By.ID, "com.tpshop.malls:id/btn_login"
 
+    @allure.step('登录页面中：输入用户名')
     def input_username(self, text):
         self.input(self.username_edit_text, text)
 
+    @allure.step('登录页面中：输入密码')
     def input_password(self, text):
         self.input(self.password_edit_text, text)
 
+    @allure.step('登录页面中：点击注册按钮')
     def click_login(self):
         self.click(self.login_button)
--- page/mine_page.py
+++ page/mine_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from base.base_action import BaseAction
@@ -6,5 +7,6 @@ from base.base_action import BaseAction
 class MinePage(BaseAction):
     login_reg_button = By.XPATH, "//*[@text='登录/注册']"
 
+    @allure.step('点击我的页面中登录/注册')
     def click_login_reg(self):
         self.click(self.login_reg_button)
