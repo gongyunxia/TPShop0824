771b899393fc3735572d170a6dc78ac620fe5e20
gongyunxia <18952555601@163.com>
2018-08-25 19:11:54 +0800

    随机生成密码，添加截图到报告

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,9 +1,30 @@
+import random
+from time import sleep
+
+import allure
 import pytest
+from allure.constants import AttachmentType
 from selenium.webdriver.common.by import By
 
 from base.base_analyze import analyze_with_file
 from base.base_driver import init_driver
 from page.page import Page
+
+
+def random_password():
+    password = ""
+    for i in range(8):
+        password += str(random.randint(0, 9))
+    print(password)
+    return password
+
+
+def show_password_data():
+    temp_list = list()
+    for i in range(2):
+        temp_list.append(random_password())
+    print(temp_list)
+    return temp_list
 
 
 class TestLogin:
@@ -33,15 +54,16 @@ class TestLogin:
     #     self.page.mine_page.click_login_reg()
     #     self.page.login_page.input_username(username)
     #     self.page.login_page.input_password(password)
-    #     return not self.page.login_page.is_login_button_enabled()
+    #     assert not self.page.login_page.is_login_button_enabled()
 
-    def test_show_password(self):
-        password = "111111"
-        password_location = (By.ID, "//*[@text='%s']" % password)
+    @pytest.mark.parametrize("password", show_password_data())
+    def test_show_password(self, password):
+        password_location = (By.XPATH, "//*[@text='%s']" % password)
         self.page.home_page.click_mine()
         self.page.mine_page.click_login_reg()
         self.page.login_page.input_password(password)
         assert not self.page.login_page.is_location_exist(password_location)
         self.page.login_page.click_view_pwd()
+        sleep(2)
+        allure.attach("显示密码：", self.driver.get_screenshot_as_png(), AttachmentType.PNG)
         assert self.page.login_page.is_location_exist(password_location)
-
